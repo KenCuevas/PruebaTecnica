1d7ee2f0b827e91eb525ec74d20ea32edcd387a8
KenCuevas <k.cuevas@unapec.edu.do>
2021-11-26 15:37:55 -0400

    Creacion del frontend para consumir web api

---
+++ BookUICrud/Data/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookUICrud.Data
+{
+    public class Response
+    {
+        public int Success { get; set; }
+        public String Message { get; set; }
+        public List<Book> Data { get; set; }
+    }
+}
